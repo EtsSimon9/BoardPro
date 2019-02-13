024db04caf5168d192fccb36e3b75747bdefd736
Simon Beaulieu <Simon Beaulieu@LAPTOP-Simon>
2019-02-13 14:51:40 -0500

    Ajout de formule

--- BoardPro/src/modele/Calcul.java
+++ BoardPro/src/modele/Calcul.java
@@ -8,7 +8,13 @@ import exception.MathException;
  * 
  */
 public class Calcul {
-
+	public static final double chargeE = 1.602 *  Math.pow(10, -19);
+	public static final double masseElectron = 9.109 *  Math.pow(10, -31);
+	public static final double masseProton = 1.672 *  Math.pow(10, -27);
+	public static final double k = 9 * Math.pow(10, 9);
+	public static final double epsilon = 8.854 * Math.pow(10, -12);
+	
+	
 	public float frequenceTofrequenceAngulaire(float frequence) {
 		return (float) (2 * Math.PI * frequence);
 	}
@@ -16,7 +22,48 @@ public class Calcul {
 	public float frequenceAngulaireTofrequence(float frequenceAngulaire) {
 		return (float) (frequenceAngulaire / (2 * Math.PI));
 	}
-
+	
+	
+	public float capaciteCondensateur(float ddp, float q) throws MathException {
+		float c = 0;
+		if (ddp != 0) {
+			c = q/ddp;
+		} else {
+			throw new MathException("Charge du condensateur null!");
+		}
+		return c;
+	}
+	
+	public float chargeCondensateur(float ddp, float c) {
+		return ddp*c;
+	}
+	
+	public float ddpCondensateur(float c, float q) throws MathException {
+		float ddp = 0;
+		if (c !=0) {
+			ddp = q/c;
+		} else {
+			throw new MathException("Charge du condensateur = 0!");
+		} return ddp;
+	}
+	
+	
+	public float calculResistance(float p, float l, float a) throws MathException {
+		float r = 0;
+		if (a != 0) {
+			r = (p*l)/a;
+		} else {
+			throw new MathException("L'aire du fil (resistance) est égale à 0");
+		}
+		return r;
+		
+	}
+	
+	public float resistiviteEtTemperature(float pi,float ti, float tf, float coefThermique) {
+		float deltaT = tf - ti;
+		return pi *(1+coefThermique*deltaT);
+	}
+	
 	public float loiOhmR(float ddp, float i) throws MathException {
 		float retour;
 
@@ -43,6 +90,8 @@ public class Calcul {
 		return retour;
 	}
 
+	
+	
 	public float impedanceCircuitRLC(float resistance, float inductance, float capacite, float frequence) {
 
 		return 0;
